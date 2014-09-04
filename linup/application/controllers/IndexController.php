252d35f7fec4f1038820f13ef0b38b8f68ac610d
kleverland <kleverland@gmail.com>
2014-09-04 10:03:06 -0300

    Projeto Inicial

---
+++ linup/application/controllers/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+    	if(Zend_Auth :: getInstance()->hasIdentity()) {
+    		return $this->_helper->redirector->goToRoute(array (
+    				'controller' => 'sip',
+    				'action' => 'index'
+    		), null, true);
+    	}
+    }
+
+    public function indexAction()
+    {
+    	
+     }
+
+
+}
+
